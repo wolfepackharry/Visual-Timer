dc231efe9a38050b5f8d67abbf21832dd88dd99e
wolfepackharry <wolfepack.harry@gmail.com>
2026-07-15 13:55:41 -0400

    +TimerCounting

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello!');
+const display = document.getElementById("display");
+const minutesInput = document.getElementById("minutes");
+const startBtn = document.getElementById("startBtn");
+let totalSeconds = 0;
+startBtn.addEventListener("click", () => 
+{
+    console.log(minutesInput.value);
+    totalSeconds = minutesInput.value * 60;
+    setInterval(() => {
+        totalSeconds = totalSeconds - 1;
+        console.log(totalSeconds);
+        let seconds = totalSeconds % 60;
+        let minutes = Math.floor(totalSeconds / 60);
+        let addedMinuteText = ""
+        let addedSecondText = ""
+        if (minutes < 10){addedMinuteText = "0"}
+        if (seconds < 10){addedSecondText = "0"}
+        display.textContent = addedMinuteText + minutes + ":" + addedSecondText + seconds;
+      }, 1000);
+})
